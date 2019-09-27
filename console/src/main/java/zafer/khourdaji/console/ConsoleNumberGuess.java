0c70215a65dc8c43238fbf0d8808845af8c2dbe9
Zafer Khourdaji <khourdaji@gmail.com>
2019-09-26 21:20:57 -0400

    add and implement ConsoleNumberGuess.java

---
+++ console/src/main/java/zafer/khourdaji/console/ConsoleNumberGuess.java
@@ -0,0 +1,71 @@
+package zafer.khourdaji.console;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+import zafer.khourdaji.Game;
+import zafer.khourdaji.MessageGenerator;
+import zafer.khourdaji.NumberGenerator;
+import zafer.khourdaji.NumberGeneratorImpl;
+
+import java.util.Scanner;
+
+@Component
+public class ConsoleNumberGuess
+//        implements ApplicationListener<ContextRefreshedEvent>
+{
+
+    private static final Logger log = LoggerFactory.getLogger(ConsoleNumberGuess.class);
+
+    @Autowired
+    private Game game;
+
+    @Autowired
+    private MessageGenerator messageGenerator;
+
+    /**
+     * The context refereshed event gets called when the context is initialized
+     * and when the context is refreshed.
+     * @param event
+     */
+//    @Override
+//    public void onApplicationEvent(ContextRefreshedEvent event) {
+//        log.info("Container ready for use.");
+//    }
+
+//    @EventListener
+//    public void start(ContextRefreshedEvent event){
+//        log.info("Container is ready for use.");
+//    }
+
+    @EventListener(ContextRefreshedEvent.class)
+    public void start(){
+        log.info("Container is ready for use.");
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            System.out.println(messageGenerator.getMainMessage());
+            System.out.println(messageGenerator.getResultMessage());
+
+            int guess = scanner.nextInt();
+            scanner.nextLine();
+            game.setGuess(guess);
+            game.check();
+
+            if (game.isGameWon() || game.isGameLost())
+            {
+                System.out.println(messageGenerator.getResultMessage());
+                System.out.println("Play again y/n?");
+                String playAgainString = scanner.nextLine().trim();
+                if (!playAgainString.equalsIgnoreCase("y"))
+                {
+                    break;
+                }
+                game.reset();
+            }
+        }
+    }
+}
