badea68e588b72ce18a02f1f34c289134628973c
Zafer Khourdaji <khourdaji@gmail.com>
2019-09-26 21:02:13 -0400

    add console module

--- console/src/main/java/zafer/khourdaji/console/Main.java
+++ console/src/main/java/zafer/khourdaji/console/Main.java
@@ -1,10 +1,13 @@
-package zafer.khourdaji;
+package zafer.khourdaji.console;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import zafer.khourdaji.AppConfig;
+import zafer.khourdaji.Game;
+import zafer.khourdaji.MessageGenerator;
+import zafer.khourdaji.NumberGenerator;
 
 public class Main {
 
